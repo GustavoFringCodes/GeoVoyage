1d3783bfb1e529bcd7145ec414bad095ad7ceb2e
Gustavo Fring <gustavogusfring123@gmail.com>
2025-07-18 19:00:15 +0400

    Update User.cs

--- GeoVoyage/Models/User.cs
+++ GeoVoyage/Models/User.cs
@@ -3,8 +3,9 @@
     public class User
     {
         public int Id { get; set; }
-        public string Image { get; set; }
+        public string ImageUrl { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Country { get; set; } 
     }
 }
